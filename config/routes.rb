5cfdcc0fea0e97c07e66d3c868217218c90b7a7d
Nick Ray <nickray22@yahoo.com>
2015-02-09 15:18:40 -0500

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, :only => [:new, :create, :destroy, :index] do
+    resources :shouts, :only => [:new, :create, :show, :destroy, :index]
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
