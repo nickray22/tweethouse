ad4d0ca7e802844c5af3e44595b2afddec4784f3
William Jones <williammasonjones@gmail.com>
2015-02-11 17:44:57 -0500

    updated to Brit's fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,30 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :shouts
 
+  has_many :relationships, foreign_key: "follower_id"
+  has_many :following, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+           class_name: "Relationship"
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+
   validates :username, presence: true, uniqueness: true,
             format: { with: /[a-zA-Z0-9]{4,20}/,
                       message: "Must be between 4 and 20 characters."}
+
+
+  def follow!(user)
+    self.following.where("relationships.followed_id => ?", user.id).first_or_create!
+  end
+
+  def unfollow!(user)
+    # NOTE: This throws an exception if we aren't following user
+    self.following.where("relationships.followed_id" => user.id).destroy!
+  end
+
+  def follows?(user)
+    self.following.include?(user)
+  end
+
 end
